32dedec115c122394d84e344b9a0e265baf491ce
Kazackoffyury <kazachkoff.yury@yandex.ru>
2021-12-07 11:08:57 +0300

    initialize

---
+++ src/main/java/ru/skypro/empoyeebook/EmpoyeeBookApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.empoyeebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpoyeeBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpoyeeBookApplication.class, args);
+    }
+
+}
